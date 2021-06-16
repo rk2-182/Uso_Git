38de71719f9e89e477de5f5a4665c233f242fd0c
rk2-182 <ricardoplus@live.cl>
2021-06-16 16:14:04 -0400

    Uso de Git 1

---
+++ hello.py
@@ -0,0 +1,21 @@
+print("********** Pytyhon 16-06-21 **********")
+print("Hello World!")
+print("**************************************")
+print("""
+	12:44 16-06-2021|	CREANDO REPOSITORIO LOCAL
+***********************************************************************************************************************************************
+
+git config: puede ser usado para establecer una configuración específica de usuario, como el email, nombre de usuario y tipo de formato, etc.
+ej:
+	git config --global user.email tuemail@ejemplo.com
+
+git init : creará un nuevo repositorio local GIT
+git status -s :muestra la lista de los archivos que se han cambiado junto con los archivos que están por ser preparados o confirmados.
+git add . :se usa para agregar archivos al área de preparación
+git commit: creará una instantánea de los cambios y la guardará en el directorio git.
+git log --oneline: muestra todas las commits en el historial del repositorio
+""")
+print()
+print("----------------Subir archivos desde repositorio local a github------------------------------")
+print("Subir archivo a repositorio github: git remote add origin https://github.com/rk2-182/UsoDeGit.git")
+print("Actualizar archivos en repositorio: git push -u origin main")
